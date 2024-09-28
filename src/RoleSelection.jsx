658af6d582f9ca8199df6029e9a92f17457fabaa
Rahulpastam <pastam.rahul@gmail.com>
2024-09-29 01:09:22 +0530

    dashboard-1

---
+++ src/RoleSelection.jsx
@@ -0,0 +1,50 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./App.css";
+import Context from "./main"
+
+const RoleSelection = () => {
+  const navigate = useNavigate();
+
+  const handleRoleSelection = (role) => {
+    if (role === "Admin") {
+      navigate("/login");
+    } else if (role === "Doctor") {
+      navigate("/doctorlogin");
+    }
+  };
+
+  return (
+    <>
+      <div className="role-selection-container">
+        <div className="logo-section">
+          <img src="logo.png" alt="Website Logo" className="website-logo" />
+        </div>
+        <h1 className="title">WELCOME TO RP HOSPITAL MANAGEMENT SYSTEM</h1>
+        <div className="roles-container">
+          <div className="role-section">
+            <img src="admin11.png" alt="Admin Logo" className="role-image" />
+            <button
+              onClick={() => handleRoleSelection("Admin")}
+              className="role-button"
+            >
+              Login as Admin
+            </button>
+          </div>
+          <div className="role-section">
+            <img src="doc.png" alt="Doctor Logo" className="role-image" />
+            <button
+              onClick={() => handleRoleSelection("Doctor")}
+              className="role-button"
+            >
+              Login as Doctor
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default RoleSelection;
---
+++ src/components/DoctorLogin.jsx
@@ -0,0 +1,78 @@
+/* eslint-disable no-unused-vars */
+import React, { useContext, useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { Context } from "../main";
+import axios from "axios";
+
+const DoctorLogin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { isAuthenticated, setIsAuthenticated, role, setRole } =
+    useContext(Context);
+
+  const navigateTo = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await axios
+        .post(
+          "https://rp-hms-backend-1.onrender.com/api/v1/user/login",
+          { email, password, role: "Doctor" },
+          {
+            withCredentials: true,
+            headers: { "Content-Type": "application/json" },
+          }
+        )
+        .then((res) => {
+          toast.success(res.data.message);
+          setIsAuthenticated(true);
+          setRole("doctor");
+          navigateTo("/");
+          setEmail("");
+          setPassword("");
+        });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+
+  if (isAuthenticated) {
+    return <Navigate to={"/"} />;
+  }
+
+  return (
+    <>
+      <section className="container form-component">
+        <img
+          style={{ width: 250, height: 250 }}
+          src="/logo.png"
+          alt="logo"
+          className="logo"
+        />
+        <h1 className="form-title">WELCOME TO RP HOSPITAL MANAGEMENT SYSTEM</h1>
+        <p>Only Doctors Are Allowed To Access These Resources!</p>
+        <form onSubmit={handleLogin}>
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div style={{ justifyContent: "center", alignItems: "center" }}>
+            <button type="submit">Login</button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+};
+
+export default DoctorLogin;
